4fe2ca03e873e8181fe65f134d3b0357ac640faa
Sahil9911 <sahilkanger21@gmail.com>
2020-10-22 00:30:42 +0530

    first commit

---
+++ screens/OnboardingScreen.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+
+import Onboarding from 'react-native-onboarding-swiper';
+
+const Dots = ({selected}) => {
+    let backgroundColor;
+
+    backgroundColor = selected ? 'rgba(0, 0, 0, 0.8)' : 'rgba(0, 0, 0, 0.3)';
+
+    return (
+        <View 
+            style={{
+                width:6,
+                height: 6,
+                marginHorizontal: 3,
+                backgroundColor
+            }}
+        />
+    );
+}
+
+const Skip = ({...props}) => (
+    <TouchableOpacity
+        style={{marginHorizontal:10}}
+        {...props}
+    >
+        <Text style={{fontSize:16}}>Skip</Text>
+    </TouchableOpacity>
+);
+
+const Next = ({...props}) => (
+    <TouchableOpacity
+        style={{marginHorizontal:10}}
+        {...props}
+    >
+        <Text style={{fontSize:16}}>Next</Text>
+    </TouchableOpacity>
+);
+
+const Done = ({...props}) => (
+    <TouchableOpacity
+        style={{marginHorizontal:10}}
+        {...props}
+    >
+        <Text style={{fontSize:16}}>Done</Text>
+    </TouchableOpacity>
+);
+
+const OnboardingScreen = ({navigation}) => {
+    return (
+        <Onboarding
+        SkipButtonComponent={Skip}
+        NextButtonComponent={Next}
+        DoneButtonComponent={Done}
+        DotComponent={Dots}
+        onSkip={() => navigation.replace("Login")}
+        onDone={() => navigation.navigate("Login")}
+        pages={[
+          {
+            backgroundColor: '#fdeb93',
+            image: <Image source={require('../assets/onboarding-img1.png')} />,
+            title: 'Connect to the World',
+            subtitle: 'Get personalized, hyper-local, real-time data on day to day basis',
+          },
+          {
+            backgroundColor: '#a6e4d0',
+            image: <Image source={require('../assets/onboarding-img2.png')} />,
+            title: 'Get your Answers',
+            subtitle: 'Post your need as a query, and get information back from people around you',
+          },
+          {
+            backgroundColor: '#e9bcbe',
+            image: <Image source={require('../assets/onboarding-img3.png')} />,
+            title: 'Help Others',
+            subtitle: "Share your experience as information, help others and build community",
+          },
+        ]}
+      />
+    );
+};
+
+export default OnboardingScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  },
+});
